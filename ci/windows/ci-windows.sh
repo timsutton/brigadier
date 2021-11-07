e475ef67e87b6d30c5596b68a28423f8d37a8ab4
Timothy Sutton <tim@synthist.net>
2021-11-07 18:17:27 -0500

    python 2?

--- ci/windows/ci-windows.sh
+++ ci/windows/ci-windows.sh
@@ -2,12 +2,14 @@
 
 set -eux -o pipefail
 
-script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd )"
+PYTHON_VERSION=2.7.18
 
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd )"
+python="/c/hostedtoolcache/windows/Python/${PYTHON_VERSION}/x64/python"
 ls '/c/program files'
 
-python -V
+$python -V
 
-which python
+# which python
 
 pip install -r "${script_dir}/requirements.txt"

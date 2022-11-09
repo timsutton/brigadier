ba6286a9e753fef6f3329c8b34f014e86cc683bb
Timothy Sutton <tim@synthist.net>
2022-11-09 09:35:48 -0500

    test

--- ci/windows/ci-windows.sh
+++ ci/windows/ci-windows.sh
@@ -6,13 +6,15 @@ PYTHON_VERSION=2.7.18
 
 script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd )"
 python="/c/hostedtoolcache/windows/Python/${PYTHON_VERSION}/x64/python"
-ls '/c/program files'
+
 export PYTHONPATH="${python}"
 $python -V
-
 $python -m pip install -r "${script_dir}/requirements.txt"
 
+# pip-installed exes will be installed here, so we'll put those at the front
+# of the PATH
 PATH="/c/hostedtoolcache/windows/Python/${PYTHON_VERSION}/x64/Scripts:$PATH"
+
 pyinstaller \
     --onefile \
     brigadier

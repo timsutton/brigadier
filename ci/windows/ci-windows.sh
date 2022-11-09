ce2e9276c82d03bcd02ad223ebb725d67cc395a1
Timothy Sutton <tim@synthist.net>
2022-11-09 09:23:47 -0500

    fix PYTHONPATH

--- ci/windows/ci-windows.sh
+++ ci/windows/ci-windows.sh
@@ -7,12 +7,11 @@ PYTHON_VERSION=2.7.18
 script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd )"
 python="/c/hostedtoolcache/windows/Python/${PYTHON_VERSION}/x64/python"
 ls '/c/program files'
-
+export PYTHONPATH="${python}"
 $python -V
 
 $python -m pip install -r "${script_dir}/requirements.txt"
 
-PYTHON_HOME="/c/hostedtoolcache/windows/Python/${PYTHON_VERSION}/x64" \
-    pyinstaller \
+pyinstaller \
     --onefile \
     brigadier
